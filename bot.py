be8547f3606c0769bc29f2fb6e135a43b765defd
1895caio <caiorodrigogui2@gmail.com>
2025-02-27 21:27:30 -0300

    Atualizei as mensagens do bot e as dependências

--- bot.py
+++ bot.py
@@ -1,42 +1,41 @@
 import os
 from telegram import Update
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
+from telegram.ext import Application, CommandHandler, MessageHandler, filters, CallbackContext
 
 # Token do seu bot no Telegram
-TELEGRAM_TOKEN = 'HTTP API:7126188971:AAG1btnbPclPYDTCE_riTmsPVP7PjMnX3Zg'
+TELEGRAM_TOKEN = os.getenv('HTTP API:7126188971:AAG1btnbPclPYDTCE_riTmsPVP7PjMnX3Zg')
 
 # Dados do PIX (você pode personalizar)
 CHAVE_PIX = '30039c5d-c603-472c-82c0-3b956ccc2298'
-VALOR_PIX = 'R$9.90'  # Valor sugerido
+VALOR_PIX = 'R$ 9.80'  # Valor sugerido
 
 # Comando /start
-def start(update: Update, context: CallbackContext):
+async def start(update: Update, context: CallbackContext):
     mensagem = f"""
-    Olá! Para acessar o melhor grupo privado, com mais de 1000 mídias selecionadas realize o pagamento via PIX para a chave:
+    Olá! Para ter acesso ao melhor grupo do telegram com mais de mil mídias totalmente selecionadas por mim, realize o pagamento via PIX para a chave:
     
     **Chave PIX:** {CHAVE_PIX}
     **Valor:** R$ {VALOR_PIX}
     
-    Após o pagamento, enviar o comprovante para ter acesso ao melhor grupo do telegram.
+    Após o pagamento, envie o comprovante para validar o seu acesso ao grupo.
     """
-    update.message.reply_text(mensagem)
+    await update.message.reply_text(mensagem)
 
 # Função para lidar com mensagens (opcional)
-def handle_message(update: Update, context: CallbackContext):
-    update.message.reply_text("Obrigado pela mensagem! Entrarei em contato assim que confirmar o pagamento.")
+async def handle_message(update: Update, context: CallbackContext):
+    await update.message.reply_text("Obrigado pela mensagem! Entrarei em contato assim que confirmar o pagamento.")
 
 # Configuração do bot
 def main():
-    updater = Updater(TELEGRAM_TOKEN)
-    dispatcher = updater.dispatcher
+    application = Application.builder().token(TELEGRAM_TOKEN).build()
 
     # Adiciona os handlers
-    dispatcher.add_handler(CommandHandler("start", start))
-    dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, handle_message))
+    application.add_handler(CommandHandler("start", start))
+    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
 
     # Inicia o bot
-    updater.start_polling()
-    updater.idle()
+    application.run_polling()
 
 if __name__ == '__main__':
     main()
+
